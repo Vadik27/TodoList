1ffe68f5bd26f7a4e02aa36b6a48a07beb3241cf
Vadim Kravtsov <vadik27k@yandex.ru>
2020-03-11 18:53:27 +0200

    install styled-component
    *create TodoMainBlock

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import TodoMainBlock from "./TodoMainBlock"
 
-const Header = () => {
+const Header = styled.div`
+background-color: blue;
+width: 80%;
+min-height: 100px;
+margin: 30px auto;
+color: white;
+`;
+
+const HeaderContainer = (pros) => {
     return (
-        <header>
-            TODO-LIST
-        </header>
+        <div>
+            <Header>
+                MY_TODO_LIST
+            </Header>
+            <TodoMainBlock />
+        </div>
     )
 };
 
-export default Header;
+export default HeaderContainer;
+
---
+++ src/components/TodoMainBlock.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+import styled, {css} from "styled-components";
+
+const Button = styled.button`
+background: blue;
+height: 30px;
+width: 100px;
+border-radius: 5px;
+border: 2px solid #5F9EA0;
+margin: 0 1em;
+padding: 0.25em 1em;
+${props => props.primary &&
+    css`
+      background: blue;
+      color: white;
+    `};
+:hover {
+    color: red;
+}
+:focus {
+    outline: none;
+}
+`;
+
+const MainBlock = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: space-around;
+`;
+
+const Input = styled.input`
+    border: 0;
+    outline: 0;
+    border-bottom: 2px solid blue;
+    height: 30px;
+    width: 100%;
+`;
+
+const TodoMainBlock = (pros) => {
+    return (
+        <div>
+            <MainBlock>
+                <div><Input type="text"/></div>
+                <div><Button primary>Add</Button></div>
+            </MainBlock>
+        </div>
+    )
+}
+
+export default TodoMainBlock;
+
